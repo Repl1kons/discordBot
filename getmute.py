407d811e4ee77c6e8325d102d61dd8995b30fe99
Garnlzerx <dubovkonstantyn@yandex.ru>
2024-01-23 00:21:22 +0300

    new epoch

--- getmute.py
+++ getmute.py
@@ -2,20 +2,26 @@ import discord
 from discord.ext import commands
 
 
-
 class get_mute(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="Mute", help="Mute people (Only admin", aliases=["mute"])
+    @commands.command(name="Mute",
+                      help="Mute people (Only admin",
+                      aliases=["mute"])
     @commands.has_role('Admin')
     async def mute(self, ctx, member: discord.Member, *, reason="Матерился"):
         guild = ctx.guild
         mutedrole = discord.utils.get(guild.roles, name="Muted")
-        emb = discord.Embed(title=f"Muted по причине: {reason}", colour=discord.Colour.green())
+        emb = discord.Embed(
+            title=f"Muted по причине: {reason}",
+            colour=discord.Colour.green())
         emb.set_author(name=member.name, icon_url=member.avatar_url)
         emb.add_field(name="Muted", value=f"Mute user {member.mention}")
-        emb.set_footer(text='Был наказан администратором {}'.format(ctx.author.name), icon_url=ctx.author.avatar_url)
+        emb.set_footer(
+            text='Был наказан администратором {}'.format(
+                ctx.author.name),
+            icon_url=ctx.author.avatar_url)
 
         if not mutedrole:
             mutedrole = await guild.create_role(name="Muted")
@@ -28,23 +34,34 @@ class get_mute(commands.Cog):
         await ctx.send(embed=emb)
         await member.send(embed=emb)
 
-    @commands.command(name="Unmute", help="Unmute people (Only admin", aliases=["unmute"])
+    @commands.command(name="Unmute",
+                      help="Unmute people (Only admin",
+                      aliases=["unmute"])
     @commands.has_role('Admin')
     async def unmute(self, ctx, member: discord.Member):
         emb = discord.Embed(title=f"Unmuted", colour=discord.Colour.green())
         emb.set_author(name=member.name, icon_url=member.avatar_url)
         emb.add_field(name="Unmute", value=f"Unmute user {member.mention}")
-        emb.set_footer(text='Был помилован администратором {}'.format(ctx.author.name), icon_url=ctx.author.avatar_url)
+        emb.set_footer(
+            text='Был помилован администратором {}'.format(
+                ctx.author.name),
+            icon_url=ctx.author.avatar_url)
 
         emb1 = discord.Embed(title=f"Unmuted", colour=discord.Colour.green())
         emb1.set_author(name=member.name, icon_url=member.avatar_url)
-        emb1.add_field(name=f"Unmute на сервере: {ctx.guild.name}", value=f"Unmute user {member.mention}")
-        emb1.set_footer(text='Был помилован администратором {}'.format(ctx.author.name), icon_url=ctx.author.avatar_url)
+        emb1.add_field(
+            name=f"Unmute на сервере: {ctx.guild.name}",
+            value=f"Unmute user {member.mention}")
+        emb1.set_footer(
+            text='Был помилован администратором {}'.format(
+                ctx.author.name),
+            icon_url=ctx.author.avatar_url)
 
         mutedrole = discord.utils.get(ctx.guild.roles, name="Muted")
         await member.remove_roles(mutedrole)
         await ctx.send(embed=emb)
         await member.send(embed=emb1)
+
 
 def setup(client):
     client.add_cog(get_mute(client))
--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import os
 import discord
 from discord.ext import commands
 from extenshions.program import settings
+from cogs import levels, wanted
 
 bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 bot.remove_command("help")
+
+
+@bot.command(name='ping')
+async def ping(ctx):
+    ping = bot.latency
+    emb = discord.Embed(description="Ща сек...", colour=discord.Color.orange())
+    msg = await ctx.send(embed=emb)
+    emb = discord.Embed(description=f'Pong! `{ping * 1000:.0f}ms` :ping_pong:', colour=discord.Color.orange())
+    await msg.edit(embed=emb)
+    print(f'[Logs] На данный момент пинг == {ping * 1000:.0f}ms | ping')
 
 class MyHelp(commands.HelpCommand):
     """My very own help formatter"""
@@ -18,7 +29,8 @@ class MyHelp(commands.HelpCommand):
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f"cogs.{filename[:-3]}")
-
-
-
+        # bot.add_cog(f"cogs.{filename[:-3]}")
+#
+# bot.add_cog(levels.Levelsys(bot))
+# bot.add_cog(wanted.WantedUser(bot))
 bot.run(settings.token)
